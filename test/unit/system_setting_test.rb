094d2034c20ca37e5d89cb32d37f42614dcb447a
Peter Donald <peter@realityforge.org>
2007-05-23 08:04:26 +0000

    Add in test for system_setting object

---
+++ test/unit/system_setting_test.rb
@@ -0,0 +1,39 @@
+#
+#  This file is part of the Jikes RVM project (http://jikesrvm.org).
+#
+#  This file is licensed to You under the Common Public License (CPL);
+#  You may not use this file except in compliance with the License. You
+#  may obtain a copy of the License at
+#
+#      http://www.opensource.org/licenses/cpl1.0.php
+#
+#  See the COPYRIGHT.txt file distributed with this work for information
+#  regarding copyright ownership.
+#
+require File.dirname(__FILE__) + '/../test_helper'
+
+class SystemSettingTest < Test::Unit::TestCase
+   def test_basic_load
+      system_setting = system_settings(:s_environment)
+      assert_equal( 1, system_setting.id )
+      assert_equal( 'environment', system_setting.name )
+      assert_equal( 'test', system_setting.value )
+      assert_equal( true, system_setting.valid? )
+   end
+
+   def test_array_read
+      assert_equal( 'test', SystemSetting['environment'] )
+   end
+
+   def test_array_write_for_existing
+      assert_equal( 'test', SystemSetting['environment'] )
+      SystemSetting['environment'] = '42'
+      assert_equal( '42', SystemSetting['environment'] )
+   end
+
+   def test_array_write_for_new
+      assert_nil( SystemSetting['flux capacitor'] )
+      SystemSetting['flux capacitor'] = '42'
+      assert_equal( '42', SystemSetting['flux capacitor'] )
+   end
+end
