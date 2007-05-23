fcb17fe11e0d7f8487e2ab222849dcfe48df94b1
Peter Donald <peter@realityforge.org>
2007-05-23 05:05:50 +0000

    Add in named route for administrators url

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
 
-  # Sample of named route:
-  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
+  map.administrators 'security/administrators', :controller => 'security', :action => 'administrators'
 
   # You can have the root of your site routed by hooking up ''
   # -- just remember to delete public/index.html.
