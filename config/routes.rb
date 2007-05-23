b85b99b7dd268605e3566d3e25f1fe3d92ae6461
Peter Donald <peter@realityforge.org>
2007-05-23 13:50:00 +0000

    Clean up the URL space where resources are being accessed

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,15 @@
 #  regarding copyright ownership.
 #
 ActionController::Routing::Routes.draw do |map|
+  map.connect 'host/:host_id/test_run/:test_run_id/test_configuration/:test_configuration_id/group/:group_id/test_case/:id/Result.txt', :controller => 'test_case', :action => 'show_output'
+  map.connect 'host/:host_id/test_run/:test_run_id/test_configuration/:test_configuration_id/group/:group_id/test_case/:id', :controller => 'test_case', :action => 'show'
+  map.connect 'host/:host_id/test_run/:test_run_id/test_configuration/:test_configuration_id/group/:id', :controller => 'group', :action => 'show'
+  map.connect 'host/:host_id/test_run/:test_run_id/test_configuration/:id', :controller => 'test_configuration', :action => 'show'
+  map.connect 'host/:host_id/test_run/:test_run_id/build_run/:id/Result.txt', :controller => 'build_run', :action => 'show_output'
+  map.connect 'host/:host_id/test_run/:test_run_id/build_run/:id', :controller => 'build_run', :action => 'show'
+  map.connect 'host/:host_id/test_run/:id', :controller => 'test_run', :action => 'show'
+  map.connect 'host/:id', :controller => 'host', :action => 'show', :id => /\d/
+
   map.administrators 'security/administrators', :controller => 'security', :action => 'administrators'
   map.connect '', :controller => 'dashboard'
 
