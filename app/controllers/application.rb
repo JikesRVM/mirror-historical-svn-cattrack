4f4d460f0d725fe2a7e3b60b5a121b553025d386
Peter Donald <peter@realityforge.org>
2007-05-23 04:21:39 +0000

    Update cookie name

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,5 +12,5 @@
 #
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_clean_cattrack_session_id'
+  session :session_key => '_cattrack_session_id'
 end
