6a51163c950153793ce2db6a41b9e0e19d63ec86
Peter Donald <peter@realityforge.org>
2007-06-06 02:02:26 +0000

    Display data on orphaned and historic data in OLAP database

--- app/controllers/admin/sysinfo_controller.rb
+++ app/controllers/admin/sysinfo_controller.rb
@@ -11,6 +11,51 @@
 #  regarding copyright ownership.
 #
 class Admin::SysinfoController < Admin::BaseController
+  verify :method => :get, :only => [:show], :redirect_to => {:action => :index}
+  verify :method => :post, :only => [:purge_stale_sessions, :purge_historic_result_facts], :redirect_to => {:action => :index}
+
   def show
+    @orhpan_dimensions = dimension_data
+  end
+
+  def purge_stale_sessions
+    SessionCleaner::remove_stale_sessions
+    redirect_to(:action => 'show')
+  end
+
+  def purge_historic_result_facts
+    ResultFact.destroy_all(['source_id IS NULL'])
+    redirect_to(:action => 'show')
+  end
+
+  def purge_historic_statistic_facts
+    StatisticFact.destroy_all(['source_id IS NULL'])
+    redirect_to(:action => 'show')
+  end
+
+  def purge_orphan_dimensions
+    Dimensions.collect do |d|
+      name = d.name[0, d.name.length - 9]
+      column_name = name.tableize.singularize + "_id"
+      sql = []
+      sql << ((d != StatisticDimension) ? "id NOT IN (SELECT DISTINCT #{column_name} FROM result_facts)" : '1 = 1')
+      sql << ((d != ResultDimension) ? "id NOT IN (SELECT DISTINCT #{column_name} FROM statistic_facts)" : '1 = 1')
+      d.destroy_all(sql.join(' AND '))
+    end
+    redirect_to(:action => 'show')
+  end
+
+  private
+
+  Dimensions = [HostDimension, TestRunDimension, TestConfigurationDimension, BuildConfigurationDimension, BuildTargetDimension, TestCaseDimension, TimeDimension, RevisionDimension, ResultDimension, StatisticDimension]
+
+  def dimension_data
+    Dimensions.collect do |d|
+      name = d.name[0, d.name.length - 9]
+      column_name = name.tableize.singularize + "_id"
+      result_fact_count = (d != StatisticDimension) ? d.count(:conditions => "id NOT IN (SELECT DISTINCT #{column_name} FROM result_facts)") : 0
+      statistic_fact_count = (d != ResultDimension) ? d.count(:conditions => "id NOT IN (SELECT DISTINCT #{column_name} FROM statistic_facts)") : 0
+      [name, result_fact_count + statistic_fact_count, d]
+    end
   end
 end
--- test/functional/admin/sysinfo_controller_test.rb
+++ test/functional/admin/sysinfo_controller_test.rb
@@ -34,4 +34,68 @@ class Admin::SysinfoControllerTest < Test::Unit::TestCase
     assert_nil(flash[:alert])
     assert_nil(flash[:notice])
   end
+
+  def test_purge_stale_sessions
+    time = Time.at(0)
+    CGI::Session::ActiveRecordStore::Session.create!( :sessid => 'abcde', :data => '', :created_on => time, :updated_on => time )
+    assert_equal(1, CGI::Session::ActiveRecordStore::Session.count)
+    post(:purge_stale_sessions, {}, {:user_id => 1})
+    assert_redirected_to(:action => 'show')
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+    assert_equal(0, CGI::Session::ActiveRecordStore::Session.count)
+  end
+
+  def test_purge_historic_result_facts
+    assert_equal([1, 2], ResultFact.find(:all, :order => 'id').collect{|r|r.id})
+    post(:purge_historic_result_facts, {}, {:user_id => 1})
+    assert_redirected_to(:action => 'show')
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+    assert_equal([1], ResultFact.find(:all, :order => 'id').collect{|r|r.id})
+  end
+
+  def test_purge_historic_statistic_facts
+    assert_equal([1, 2], StatisticFact.find(:all, :order => 'id').collect{|r|r.id})
+    post(:purge_historic_statistic_facts, {}, {:user_id => 1})
+    assert_redirected_to(:action => 'show')
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+    assert_equal([1], StatisticFact.find(:all, :order => 'id').collect{|r|r.id})
+  end
+
+  def test_purge_orphan_dimensions
+    assert_equal(
+    [
+    ["Host", 2, HostDimension],
+    ["TestRun", 0, TestRunDimension],
+    ["TestConfiguration", 6, TestConfigurationDimension],
+    ["BuildConfiguration", 4, BuildConfigurationDimension],
+    ["BuildTarget", 0, BuildTargetDimension],
+    ["TestCase", 2, TestCaseDimension],
+    ["Time", 2, TimeDimension],
+    ["Revision", 2, RevisionDimension],
+    ["Result", 2, ResultDimension],
+    ["Statistic", 0, StatisticDimension],
+    ],
+    @controller.send(:dimension_data))
+    post(:purge_orphan_dimensions, {}, {:user_id => 1})
+    assert_redirected_to(:action => 'show')
+    assert_nil(flash[:alert])
+    assert_nil(flash[:notice])
+    assert_equal(
+    [
+    ["Host", 0, HostDimension],
+    ["TestRun", 0, TestRunDimension],
+    ["TestConfiguration", 0, TestConfigurationDimension],
+    ["BuildConfiguration", 0, BuildConfigurationDimension],
+    ["BuildTarget", 0, BuildTargetDimension],
+    ["TestCase", 0, TestCaseDimension],
+    ["Time", 0, TimeDimension],
+    ["Revision", 0, RevisionDimension],
+    ["Result", 0, ResultDimension],
+    ["Statistic", 0, StatisticDimension]
+    ],
+    @controller.send(:dimension_data))
+  end
 end
