8a3051e9dbc371fce6eefbd6ab417219cbd02c08
Peter Donald <peter@realityforge.org>
2007-06-15 14:59:37 +0000

    Minor modifications to get working again

--- app/controllers/explorer/report_controller.rb
+++ app/controllers/explorer/report_controller.rb
@@ -11,30 +11,29 @@
 #  regarding copyright ownership.
 #
 class Explorer::ReportController < Explorer::BaseController
-  #verify :method => :get, :only => [:list], :redirect_to => :access_denied_url
-
   def list
     Olap::Query::Filter::AutoFields.each do |f|
       value = f.dimension.attribute_values(f.name.to_s)
       instance_variable_set("@#{f.key.to_s.pluralize}", value)
     end
-    @presentations = Olap::Query::Presentation.find(:all, :order => 'name')
-    query = Olap::Query::Query.new
+    query = Olap::Query::Query.new(params[:query])
     query.filter = Olap::Query::Filter.new(params[:filter])
     query.filter.name = '*'
     query.filter.description = ''
     query.name = '*'
     query.description = ''
 
+    @measure = query.measure
     @filter = query.filter
     @query = query
 
     if request.post?
       @presentation = Olap::Query::Presentation.find(params[:presentation])
-      if @presentation and @summarizer.valid? and @filter.valid?
+      if @presentation and query.measure.valid? and query.filter.valid?
         @results = query.perform_search
       end
     end
+    @presentations = Olap::Query::Presentation.find(:all, :order => 'name')
     @measures = Olap::Query::Measure.find(:all)
     @filters = Olap::Query::Filter.find(:all, :order => 'name')
   end
