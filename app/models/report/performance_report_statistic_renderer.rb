ac153dc3d487c0084d927fbe563ad9e8ef0f4727
Peter Donald <peter@realityforge.org>
2007-09-19 03:45:01 +0000

    Remove .rb suffix as it is not needed

--- app/models/report/performance_report_statistic_renderer.rb
+++ app/models/report/performance_report_statistic_renderer.rb
@@ -10,7 +10,7 @@
 #  See the COPYRIGHT.txt file distributed with this work for information
 #  regarding copyright ownership.
 #
-require 'RMagick.rb'
+require 'RMagick'
 require 'rvg/rvg'
 
 class Report::PerformanceReportStatisticRenderer
