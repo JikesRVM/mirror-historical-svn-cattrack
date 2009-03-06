1adda6984b93c23f5b12690f67c034ec9dc002e8
David Grove <groved@us.ibm.com>
2009-03-06 15:19:58 +0000

    Bug fix in cross-run comparision to correctly tabulate test cases that were present in the first run but not present in the second (unlike in regression_report which is time-biased, for this report we want to treate the two runs symetrically).
    Also make some minor layout and UI improvements in compare_two_runs.haml

--- app/models/report/run_comparision_report.rb
+++ app/models/report/run_comparision_report.rb
@@ -35,8 +35,7 @@ SELECT
     count(case when build_configurations.test_run_id = #{@first_run.id} then 1 else NULL end) AS in_first_run,
     count(case when build_configurations.test_run_id = #{@second_run.id} then 1 else NULL end) AS in_second_run,
     count(case when build_configurations.test_run_id = #{@first_run.id} AND test_case_executions.result = 'SUCCESS' then 1 else NULL end) AS first_run_success,
-    count(case when build_configurations.test_run_id = #{@second_run.id} AND test_case_executions.result = 'SUCCESS' then 1 else NULL end) AS second_run_success,
-    max(case when build_configurations.test_run_id = #{@first_run.id} then test_cases.id else NULL end) AS test_case_id
+    count(case when build_configurations.test_run_id = #{@second_run.id} AND test_case_executions.result = 'SUCCESS' then 1 else NULL end) AS second_run_success
 FROM test_case_executions
     LEFT JOIN test_cases ON test_case_executions.test_case_id = test_cases.id
     LEFT JOIN groups ON test_cases.group_id = groups.id
@@ -49,37 +48,37 @@ SQL
 
     first_only_pass_sql = <<SQL
 SELECT * FROM (#{sql}) f
-WHERE test_case_id IS NOT NULL AND first_run_success = 1 AND second_run_success != 1
+WHERE first_run_success = 1 AND second_run_success != 1
 ORDER BY build_configuration_name, test_configuration_name, group_name, test_case_name, test_case_execution_name
 SQL
 
     second_only_pass_sql = <<SQL
 SELECT * FROM (#{sql}) f
-WHERE test_case_id IS NOT NULL AND first_run_success != 1 AND second_run_success = 1
+WHERE first_run_success != 1 AND second_run_success = 1
 ORDER BY build_configuration_name, test_configuration_name, group_name, test_case_name, test_case_execution_name
 SQL
 
     both_pass_sql = <<SQL
 SELECT * FROM (#{sql}) f
-WHERE test_case_id IS NOT NULL AND first_run_success = 1 AND second_run_success = 1
+WHERE first_run_success = 1 AND second_run_success = 1
 ORDER BY build_configuration_name, test_configuration_name, group_name, test_case_name, test_case_execution_name
 SQL
 
     neither_pass_sql = <<SQL
 SELECT * FROM (#{sql}) f
-WHERE test_case_id IS NOT NULL AND first_run_success != 1 AND second_run_success != 1
+WHERE first_run_success != 1 AND second_run_success != 1
 ORDER BY build_configuration_name, test_configuration_name, group_name, test_case_name, test_case_execution_name
 SQL
 
     only_in_first_sql = <<SQL
 SELECT * FROM (#{sql}) f
-WHERE test_case_id IS NOT NULL AND in_first_run = 1 AND in_second_run != 1
+WHERE in_first_run = 1 AND in_second_run != 1
 ORDER BY build_configuration_name, test_configuration_name, group_name, test_case_name, test_case_execution_name
 SQL
 
     only_in_second_sql = <<SQL
 SELECT * FROM (#{sql}) f
-WHERE test_case_id IS NOT NULL AND in_first_run != 1 AND in_second_run = 1
+WHERE in_first_run != 1 AND in_second_run = 1
 ORDER BY build_configuration_name, test_configuration_name, group_name, test_case_name, test_case_execution_name
 SQL
 
